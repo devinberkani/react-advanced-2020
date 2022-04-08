376db736a6dc4a20c3edbd41bfd0577457233038
Devin Berkani <devinberkani@gmail.com>
2022-04-08 11:40:59 -0400

    completed up to reducer lessons

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,39 @@
+import { HtmlElementTypeError } from '@testing-library/jest-dom/dist/utils';
 import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  console.log(people);
+
+  // const removeItem = (id) => {
+  //   let newPeople = people.filter((person) => person.id !== id);
+  //   setPeople(newPeople);
+  // };
+
+  const removeItem = (id) => {
+    setPeople((oldPeople) => {
+      let newPeople = oldPeople.filter((person) => person.id !== id);
+      return newPeople;
+    });
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={() => setPeople([])}>
+        clear items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
