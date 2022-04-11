9f3176d8688b9003317dd1d42e2abab969024bd4
Devin Berkani <devinberkani@gmail.com>
2022-04-11 07:59:40 -0400

    complete custom hooks lesson

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,23 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = () => {};
+//url argument allows for this to be reused with different data sets
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+
+  return {
+    loading,
+    products,
+  };
+};
